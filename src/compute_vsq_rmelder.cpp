e640bd790bca2e63b69e4241d8457b9a11af04ec
robert-m-elder <rmelder@gmail.com>
2017-08-01 11:16:17 -0600

    Added compute to calculate mean squared velocity

---
+++ src/compute_vsq_rmelder.cpp
@@ -0,0 +1,67 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#include <mpi.h>
+#include "compute_vsq_rmelder.h"
+#include "atom.h"
+#include "update.h"
+#include "force.h"
+#include "domain.h"
+#include "group.h"
+#include "error.h"
+
+using namespace LAMMPS_NS;
+
+/* ---------------------------------------------------------------------- */
+
+ComputeVSQ::ComputeVSQ(LAMMPS *lmp, int narg, char **arg) :
+  Compute(lmp, narg, arg)
+{
+  if (narg != 3) error->all(FLERR,"Illegal compute vsq command");
+
+  scalar_flag = 1;
+  extscalar = 1;
+}
+
+/* ---------------------------------------------------------------------- */
+
+void ComputeVSQ::init()
+{
+  pfactor = 1.0;
+}
+
+/* ---------------------------------------------------------------------- */
+
+double ComputeVSQ::compute_scalar()
+{
+  invoked_scalar = update->ntimestep;
+
+  double **v = atom->v;
+  double *rmass = atom->rmass;
+  double *mass = atom->mass;
+  int *mask = atom->mask;
+  int *type = atom->type;
+  int nlocal = atom->nlocal;
+
+  double vsq = 0.0;
+
+  if (rmass) {
+    for (int i = 0; i < nlocal; i++)
+      if (mask[i] & groupbit)
+        vsq += (v[i][0]*v[i][0] + v[i][1]*v[i][1] + v[i][2]*v[i][2]);
+  }
+
+  MPI_Allreduce(&vsq,&scalar,1,MPI_DOUBLE,MPI_SUM,world);
+  scalar *= pfactor;
+  return scalar;
+}
